1e2ce7d7aa39f43681f20e30e1467eeff1089fe8
NonKey <newworldnewpc@gmail.com>
2025-06-22 14:51:40 +0300

    add function.py

--- function.py
+++ function.py
@@ -11,3 +11,30 @@ def get_connection():
         port=os.environ["PGPORT"]        
     )
     return conn
+
+def select_DB(what, table, join, condition):
+    conn = get_connection()
+    cursor =conn.cursor()
+
+    select = f"""
+                SELECT {what} FROM {table} {join} {condition};
+            """
+    cursor.execute(select)
+    name = cursor.fetchall()
+    conn.close()
+
+    return name
+
+def insert_DB(table, what, data):
+    conn = get_connection()
+    cursor =conn.cursor()
+
+    insert = f"""
+                INSERT INTO {table} ({what})
+                VALUES {data}
+            """
+    cursor.execute()
+    conn.commit()
+    conn.close()
+
+    return insert
